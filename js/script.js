471f4f14c5d6c300f3f47c917e194b48d7f9cb14
Никита Швец <36887588+niksvhvets@users.noreply.github.com>
2018-03-29 16:37:09 +0300

    Доделал скрипт модального окна
    1) Анимация при открытии
    2) Анимация при пустых полях + сообщение в консоле инспектора

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 var link = document.querySelector(".button-search-hotels");
 var popup = document.querySelector(".search-hotels-wrapper");
 
+
+var form = document.querySelector("form");
+var arrival = popup.querySelector("[name=arrival]");
+var departure = popup.querySelector("[name=departure]");
+
+
     link.addEventListener("click", function(event) { 
         event.preventDefault(); 
         popup.classList.toggle("search-hotels-wrapper-show");
+        arrival.focus();
+    });
+
+    form.addEventListener("submit", function (event) {
+    	if (!arrival.value || !departure.value) {     
+	    	event.preventDefault();
+		    console.log("Нужно ввести дату заезда и выезда!");
+		  //  alert("Нужно ввести дату заезда!");
+		    popup.classList.toggle("search-hotels-wrapper-error");
+		} 
+	});
+
+    window.addEventListener("keydown", function(event) {
+    	if ( event.keyCode === 27) {
+    		event.preventDefault();
+    		if (popup.classList.contains("search-hotels-wrapper-show")) {
+    			popup.classList.toggle("search-hotels-wrapper-show");
+    		}
+    	}
     });
